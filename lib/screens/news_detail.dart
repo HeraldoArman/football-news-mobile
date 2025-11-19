b103f417cae478fc748690f727e88c1f7b31df91
HeraldoArman <heraldoarman7@gmail.com>
2025-11-19 16:30:21 +0700

    chore: github workflows

--- lib/screens/news_detail.dart
+++ lib/screens/news_detail.dart
@@ -12,7 +12,7 @@ class NewsDetailPage extends StatelessWidget {
                     'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     return '${date.day} ${months[date.month - 1]} ${date.year}, ${date.hour.toString().padLeft(2, '0')}:${date.minute.toString().padLeft(2, '0')}';
   }
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
